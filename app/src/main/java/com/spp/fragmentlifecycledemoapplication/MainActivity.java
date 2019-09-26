54d55d3d383856ab1f32d442bc34ac3b69130635
studentp_10 <IamSid25>
2019-09-26 12:48:26 +0530

    Initial commit

---
+++ app/src/main/java/com/spp/fragmentlifecycledemoapplication/MainActivity.java
@@ -0,0 +1,77 @@
+package com.spp.fragmentlifecycledemoapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String COMMON_TAG = "CombinedLifeCycle";
+    private static final String ACTIVITY_NAME = MainActivity.class.getSimpleName();
+    private static final String TAG = COMMON_TAG;
+
+
+
+    private Button buttonAddFragment;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAddFragment = (Button)findViewById(R.id.buttonAddFragment);
+
+        buttonAddFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addFragment();
+            }
+        });
+        Log.i(TAG, ACTIVITY_NAME+" onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, ACTIVITY_NAME+" onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, ACTIVITY_NAME+" onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, ACTIVITY_NAME+" onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, ACTIVITY_NAME+" onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, ACTIVITY_NAME+" onDestroy");
+    }
+
+    private void addFragment(){
+        Fragment fragment=new SampleFragment();
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentContainer,fragment,"demofragment");
+        fragmentTransaction.addToBackStack("fragmentStack1");
+        fragmentTransaction.commit();
+    }
+}
